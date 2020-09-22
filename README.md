e7a2ee65c9a04738328f9db5348e4243c3dc9391
bonf <dan.bonifazi@gmail.com>
2020-09-22 10:05:21 +0200

    updated seeds + README

---
+++ README.md
@@ -0,0 +1,33 @@
+## Setup
+
+```
+rails db:create
+rails db:seed
+```
+
+
+## Links
+
+
+Main Site
+
+```
+http://localhost:3000
+```
+
+Italy mini-site
+
+```
+http://localhost:3000/it
+```
+
+Spain mini-site
+
+```
+http://localhost:3000/es
+```
+
+Admin Area
+```
+http://localhost:3000/site_admin
+```
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,22 @@
 require 'faker'
 
-def rand_time(from, to=Time.now)
-  Time.at(rand_in_range(from.to_f, to.to_f))
-end
+@en = Language.create(id: 1, name: 'English', country_code: 'gb')
+@it = Language.create(id: 2, name: 'Italian', country_code: 'it')
+@es = Language.create(id: 3, name: 'Spanish', country_code: 'es')
 
-def rand_in_range(from, to)
-  rand * (to - from) + from
-end
 
+@it_site = Site.create name: 'Vox Italy', slug: 'it'
+@es_site = Site.create name: 'Vox Spain', slug: 'es'
+
+
+# Main Site Data
+
+User.create!(
+  email: "admin@example.com",
+  password: "secret",
+  admin: true,
+  site: @it_site
+)
 
 @coordinates = [
   {x: 0.1734 , y: 0.4637},
@@ -24,23 +33,49 @@ end
                 description: Faker::Company.bs,
                 x: @coordinates[i][:x],
                 y: @coordinates[i][:y],
-                created_at: rand_time(30.days.ago),
-                updated_at: rand_time(30.days.ago),
                 hq: hq)
 end
-Language.find_or_create_by(id: 1).update(name: 'English', country_code: 'uk')
-
-Site.create name: 'Test Site'
-
-User.create!(
-  email: "admin@example.com",
-  password: "secret",
-  admin: true,
-  site: Site.first
-)
-
-
 
 4.times do |i|
   NewsItem.create title: "News #{i}", body: "<p>Example news body #{i}</p>"
 end
+
+
+# Local Sites
+
+def insert_translation(site, language)
+
+  translation = Translation.find_or_create_by({
+    site_id: site.id,
+    language_id: language.id
+  })
+
+  translation.update!(
+    intro: "#{site.name} - #{language.name} - intro",
+    services: "#{site.name} - #{language.name} - services",
+    products: "#{site.name} - #{language.name} - products",
+    find_us: "#{site.name} - #{language.name} - find_us",
+    lets_talk: "#{site.name} - #{language.name} - lets_talk",
+    support: "#{site.name} - #{language.name} - support",
+    intro_label: "#{site.name} intro",
+    services_label: "#{language.country_code} services",
+    products_label: "#{language.country_code} products",
+    news_label: "#{language.country_code} news",
+    find_us_label: "#{language.country_code} find_us",
+    lets_talk_label: "#{language.country_code} lets_talk",
+    support_label: "#{language.country_code} support",
+    contacts_label: "#{language.country_code} contacts",
+    products_link_label: "#{language.country_code} products_link",
+    services_link_label: "#{language.country_code} services_link",
+    read_more: "#{language.country_code} read_more"
+  )
+end
+
+
+insert_translation(@it_site, @en)
+insert_translation(@it_site, @it)
+insert_translation(@es_site, @en)
+insert_translation(@es_site, @es)
+
+
+
